a62913276a07546b4f389b31827c2e796b9eba9e
rakeshpaulraj <rakeshpaulraj2004@gmail.com>
2024-04-13 23:24:16 +0530

    added models

--- backend/src/models/Waste_collection.ts
+++ backend/src/models/Waste_collection.ts
@@ -1,46 +1,23 @@
 import { DataTypes, Model } from "sequelize";
 import { sequelize } from "../db";
-import { User } from "./User";
+import { User } from "./User"; 
+import { Vehicle } from "./Vehicle";
+import { Area } from "./Area";
 
-export class waste_collection  extends Model {
-    total_weight: number | undefined;
-    bio_weight: number | undefined;
-    non_bio_weight: number | undefined;
-    
-
-}
-waste_collection.init({
-    w_date:{
-        type: DataTypes.DATE,
-        allowNull: false
-    },
-    bio_weight:{
+export class waste_collection extends Model {}
+waste_collection.init ({
+    Vehicle_id:{
         type: DataTypes.INTEGER,
         allowNull: false
     },
-    non_bio_weight:{
-        type: DataTypes.INTEGER,
-        allowNull: false
-    },
-    user_id:{
-        type: DataTypes.INTEGER,
-        allowNull: false
-    },
-    total_weight:{
+    Area_id:{
         type: DataTypes.INTEGER,
         allowNull: false
     }
-},
-{
-  sequelize,
+},{
+    sequelize,
   modelName: "waste_collection",
-  timestamps: true,
+  timestamps: false,
   underscored: true,
-  tableName: "waste_collection",
-  hooks: {
-    beforeSave: (instance) => {
-
-        instance.total_weight = (instance.bio_weight || 0) + (instance.non_bio_weight || 0);
-    }
-}
+  tableName: "waste_collection"
 })
--- backend/src/models/Waste_produced.ts
+++ backend/src/models/Waste_produced.ts
@@ -1,3 +1,47 @@
 import { DataTypes, Model } from "sequelize";
 import { sequelize } from "../db";
 import { User } from "./User";
+
+
+export class waste_produced  extends Model {
+    total_weight: number | undefined;
+    bio_weight: number | undefined;
+    non_bio_weight: number | undefined;
+    
+
+}
+waste_produced.init({
+    w_date:{
+        type: DataTypes.DATE,
+        allowNull: false
+    },
+    bio_weight:{
+        type: DataTypes.INTEGER,
+        allowNull: false
+    },
+    non_bio_weight:{
+        type: DataTypes.INTEGER,
+        allowNull: false
+    },
+    user_id:{
+        type: DataTypes.INTEGER,
+        allowNull: false
+    },
+    total_weight:{
+        type: DataTypes.INTEGER,
+        allowNull: false
+    }
+},
+{
+  sequelize,
+  modelName: "waste_produced",
+  timestamps: true,
+  underscored: true,
+  tableName: "waste_produced",
+  hooks: {
+    beforeSave: (instance) => {
+
+        instance.total_weight = (instance.bio_weight || 0) + (instance.non_bio_weight || 0);
+    }
+}
+})
