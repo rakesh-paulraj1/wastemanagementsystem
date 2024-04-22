626d491c56731dc17ebcde967552b937bfc906af
rakeshpaulraj <rakeshpaulraj2004@gmail.com>
2024-04-22 09:34:07 +0530

    deleted files

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,9 +5,9 @@ import { Area } from "./models/Area";
 import { User } from "./models/User";
 import { Complaints } from "./models/Complaints";
 import { Admin } from "./models/Admin";
-import { waste_collection } from "./models/Waste_collection";
+
 import {waste_produced } from  "./models/Waste_produced";
-import { Vehicle } from "./models/Vehicle";
+
 import AdminRouter  from "./routes/Adminrouter";
 import UserRouter from "./routes/Userrouter";
 
@@ -23,9 +23,9 @@ Area.sync()
 User.sync()
 Complaints.sync() 
 Admin.sync()
-waste_collection.sync()
+
 waste_produced.sync()
-Vehicle.sync()
+
 
   .then(() => console.log("All models synced with the database"))
   .catch((err) => console.error("Unable to sync  all models:", err));
