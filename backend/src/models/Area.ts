8b814a8752c58c144a476cf5c6461a70873887c5
rakeshpaulraj <rakeshpaulraj2004@gmail.com>
2024-04-25 23:48:15 +0530

    some minor changes

--- backend/src/models/Area.ts
+++ backend/src/models/Area.ts
@@ -7,7 +7,7 @@ Area.init(
       area_id: {
         type: DataTypes.INTEGER,
         primaryKey: true,
-        autoIncrement: true
+        autoIncrement:true
       },
       area_name: {
         type: DataTypes.STRING,
--- frontend/src/commonpages/LoginUA.tsx
+++ frontend/src/commonpages/LoginUA.tsx
@@ -4,7 +4,7 @@ import {UserLogincard} from '../components/UserLogincard';
 
 export const Login = () => {
     return (<div>
-        <div className="font-bold 0 bg-clip-text text-transparent text-xl md:text-5xl mr-5 "style={{ color: 'green' }}> WASTE MAANAGEMENT SYSTEM </div>
+        <div className="font-bold 0 bg-clip-text text-transparent text-xl md:text-5xl mr-5 "style={{ color: 'green' }}> WASTE MANAGEMENT SYSTEM </div>
         <div className="p-4 max-w-7xl  mx-auto relative z-10  w-full pt-20 md:pt-0 grid grid-cols-1 md:grid-cols-2">
             <UserLogincard />
             <AdminLogincard />
